6c3fab1e760434c30d5c0ac9062ce398170072bf
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-02-17 22:08:24 +0600

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line import/prefer-default-export
 export const greetings = () => {
   const name = readlineSync.question('May I have your name? ');
   return `Hello, ${name}!`;
