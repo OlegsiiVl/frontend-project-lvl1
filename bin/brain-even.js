2f5bd5f1f896bb76b77112c72d1a5d2099964d30
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-02-18 10:51:43 +0600

    update

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,44 +1,35 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import {
-  questionAnswer, hello, incorrectUnswer, question,
+  questionAnswer, hello, question, getRandomInt,
 } from '../src/index.js';
+
 
 const brainEven = () => {
   console.log(hello());
   const name = readlineSync.question('May I have your name? ');
-  const res = [];
+  let i = 0;
   let finish = '';
-
+  let answer = '';
+  const incorrectAnswer = (a) => `'${a}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`;
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const q1 = question(13);
-  console.log(q1);
-  const q1A = questionAnswer();
-  if (q1A === 'no') {
-    res.push(1);
-    console.log('Correct!');
-  } else if (q1 !== 'no') {
-    return incorrectUnswer(name);
+  // eslint-disable-next-line no-unreachable-loop
+  while (i < 3) {
+    const number = getRandomInt(0, 100);
+    const q = question(number);
+    console.log(q);
+    const qA = questionAnswer();
+    if (number % 2 === 0) {
+      answer = 'yes';
+      i += 1;
+    } else if (number % 2 !== 0) {
+      answer = 'no';
+      i += 1;
+    }
+    // eslint-disable-next-line no-unused-expressions
+    answer === qA ? console.log('Correct!') : i -= 1 && console.log(incorrectAnswer(qA));
   }
-  const q2 = question(15);
-  console.log(q2);
-  const q2A = questionAnswer();
-  if (q2A === 'no') {
-    res.push(1);
-    console.log('Correct!');
-  } else if (q2 !== 'no') {
-    return incorrectUnswer(name);
-  }
-  const q3 = question(17);
-  console.log(q3);
-  const q3A = questionAnswer();
-  if (q3A === 'no') {
-    res.push(1);
-    console.log('Correct!');
-  } else if (q3 !== 'no') {
-    return incorrectUnswer(name);
-  }
-  if (res.length === 3) {
+  if (i === 3) {
     finish = `Congratulations, ${name}!`;
   }
   return finish;
