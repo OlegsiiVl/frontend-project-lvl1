af34e5148c6802af7d851bb04e228a26045c36cf
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-02-18 11:50:14 +0600

    add brain-calc

--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ export const questionAnswer = () => readlineSync.question('Your answer: ');
 export const incorrectUnswerCalc = (a) => `\nLet's try again, ${a}!`;
 
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const brainCalcFunc = () => {
+  const operator = ['+', '-', '*'];
+  const operand = getRandomInt(0, 100);
+  const operand1 = getRandomInt(50, 100);
+  const rand = Math.floor(Math.random() * operator.length);
+  return operand + operator[rand] + operand1;
+};
