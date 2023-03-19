637e2fc396e86515a438f138ec1842bdddd3ad2f
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-03-19 14:21:36 +0600

    update index and brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,7 +1,15 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../helpers.js';
 
-const hasPrime = (num) => num % 2 === 0;
+const hasPrime = (num) => {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num < 2) {
+      return false;
+    } if (num % i === 0) {
+      return false;
+    }
+  } return true;
+};
 const generateRoundPrime = () => {
   const number = getRandomNumber(2, 20);
   const expectedAnswer = hasPrime(number) ? 'yes' : 'no';
--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 import readlineSync from 'readline-sync';
 
-export const greetingInTheGame = () => console.log('Welcome to the Brain Games!');
-export const setQuestionGame = (a) => console.log(`Question: ${a}`);
-export const getAnswerQuestion = () => readlineSync.question('Your answer: ');
-export const incorrectAnswer = (questionAnswer, correctAnswer, getNamePlayer) => {
-  console.log(`'${questionAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${getNamePlayer}!`);
-};
-export const endGame = (getNamePlayer) => {
-  console.log(`Congratulations, ${getNamePlayer}!`);
-};
 const gameEngine = (gameCondition, gameRound) => {
-  greetingInTheGame();
+  console.log('Welcome to the Brain Games!');
   const getNamePlayer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getNamePlayer}`);
   console.log(gameCondition);
   for (let i = 0; i < 3; i += 1) {
     const startGame = gameRound();
-    setQuestionGame(startGame[0]);
-    const questionAnswer = getAnswerQuestion();
+    console.log(`Question: ${startGame[0]}`);
+    const questionAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = String(startGame[1]);
     if (questionAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return incorrectAnswer(questionAnswer, correctAnswer, getNamePlayer);
+      return console.log(`'${questionAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${getNamePlayer}!`);
     }
-  } return endGame(getNamePlayer);
+  } return console.log(`Congratulations, ${getNamePlayer}!`);
 };
 export default gameEngine;
