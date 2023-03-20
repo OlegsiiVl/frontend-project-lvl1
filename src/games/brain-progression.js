c85bf787c663e537967c86348d7c4256fe7c36a1
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-03-20 13:02:56 +0600

    update

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,27 +1,32 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../helpers.js';
 
-const arrayProgression = (progression, start, step, hiddenIndex, length) => {
+const generateProgression = (length, start, step) => {
+  const progression = [];
   for (let i = 0; i < length; i += 1) {
-    if (i === hiddenIndex) {
-      progression.push('..');
-    } else {
-      progression.push(start + i * step);
-    }
-  }
+    progression.push(start + step * i);
+  } return progression;
 };
+const progressionHiddenSymbol = (arr, hiddenIndex) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (hiddenIndex <= arr.length) {
+      arr[hiddenIndex] = '..';
+    }
+  } return arr.join(' ');
+};
+
 const descriptionProgression = 'What number is missing in the progression?';
 const generateRoundProgression = () => {
   const length = getRandomNumber(5, 10);
-  const hiddenIndex = getRandomNumber(0, length);
-  const start = getRandomNumber(0, 10);
+  const start = getRandomNumber(0, 100);
   const step = getRandomNumber(2, 5);
-  const progression = [];
-  arrayProgression(progression, start, step, hiddenIndex, length);
-  const progressions = progression.join(' ');
-  const correctAnswer = start + hiddenIndex * step;
-  return [progressions, correctAnswer];
+  const hiddenIndex = Math.floor(Math.random() * length);
+  const progression = generateProgression(length, start, step);
+  const correctAnswer = progression[hiddenIndex];
+  const question = progressionHiddenSymbol(progression, hiddenIndex);
+  return [question, correctAnswer];
 };
+
 const startBrainProgression = () => {
   gameEngine(descriptionProgression, generateRoundProgression);
 };
