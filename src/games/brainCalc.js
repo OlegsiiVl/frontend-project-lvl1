86120aa89c36c42c92dc694c77c20637b79837ac
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-03-27 22:02:41 +0600

    update brainCalc.js

--- src/games/brainCalc.js
+++ src/games/brainCalc.js
@@ -17,11 +17,13 @@ const generateRoundCalc = () => {
   const number1 = getRandomNumber(1, 15);
   const number2 = getRandomNumber(6, 10);
   const operators = ['+', '-', '*'];
+
   const randomOperator = operators[getRandomNumber(0, operators.length - 1)];
   const question = `${number1} ${randomOperator} ${number2}`;
   const answer = String(calculate(number1, randomOperator, number2));
   return [question, answer];
 };
+
 const description = 'What is the result of the expression?';
 const startBrainCalc = () => {
   runEngine(description, generateRoundCalc);
