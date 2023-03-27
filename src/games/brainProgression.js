2dc565e7886218d05c82a0efe01f121fa4a07438
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-03-27 22:55:48 +0600

    update brainProgression.js

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -5,20 +5,15 @@ const generateProgression = (length, start, step) => {
   const progression = [];
   for (let i = 0; i <= length; i += 1) {
     progression.push(start + step * i);
-    console.log(progression);
   }
   return progression;
 };
 const description = 'What number is missing in the progression?';
 const generateRoundProgression = () => {
   const length = getRandomNumber(5, 10);
-  console.log(length);
   const start = getRandomNumber(0, 100);
-  console.log(start);
   const step = getRandomNumber(2, 5);
-  console.log(step);
   const hiddenIndex = getRandomNumber(0, length);
-  console.log(hiddenIndex);
   const question = generateProgression(length, start, step);
   const answer = String(question[hiddenIndex]);
   question[hiddenIndex] = '..';
