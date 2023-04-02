d00d8c43d98ce5edf68ad15e18ff344aa7d0474e
Олег Сидоревич <olegsidorevich@MacBook-Pro-Oleg.local>
2023-04-02 15:17:15 +0600

    fix brainProgression.js

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../helpers.js';
 
 const generateProgression = (length, start, step) => {
   const progression = [];
-  for (let i = 0; i <= length; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     progression.push(start + step * i);
   }
   return progression;
@@ -13,8 +13,8 @@ const generateRoundProgression = () => {
   const length = getRandomNumber(5, 10);
   const start = getRandomNumber(0, 100);
   const step = getRandomNumber(2, 5);
-  const hiddenIndex = getRandomNumber(0, length);
   const question = generateProgression(length, start, step);
+  const hiddenIndex = getRandomNumber(0, question.length - 1);
   const answer = String(question[hiddenIndex]);
   question[hiddenIndex] = '..';
   return [question.join(' '), answer];
